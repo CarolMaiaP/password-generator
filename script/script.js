43423e25d952d6013df1b29dc759e7cafd0b8d68
Caroline Maia Abreu da Silva <caroline.silva@vnda.com.br>
2022-06-02 19:13:37 -0300

    feat: function to generate password

--- script/script.js
+++ script/script.js
@@ -1,11 +1,33 @@
 let sliderElement = document.querySelector("#slider");
-console.log(sliderElement);
 
 let sizePassword = document.querySelector("#valor");
-console.log(sizePassword);
+
+let buttonElement = document.querySelector("#button");
+console.log(buttonElement);
+
+let containerPassword = document.querySelector("#container-password");
+console.log(containerPassword);
+
+let password = document.querySelector("#password");
+console.log(password);
+
+let charset =
+  "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789!@#$%&*";
 
 sizePassword.innerHTML = sliderElement.value;
 
 slider.oninput = function () {
   sizePassword.innerHTML = this.value;
 };
+
+function generatePassword() {
+  let pass = "";
+
+  for (i = 0, n = charset.length; i < sliderElement.value; ++i) {
+    pass += charset.charAt(Math.floor(Math.random() * n));
+  }
+
+  containerPassword.classList.remove("hide");
+  console.log(pass);
+  password.innerHTML = pass;
+}
